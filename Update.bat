81ff920e5d25241d914e1a104bb5e1af24c2af01
Sublustrum007 <Sublustrum007@yandex.ru>
2026-01-27 16:39:14 +0300

    Create file Update.bat SKANER 27/01/2026 16:39:14

--- Update.bat
+++ Update.bat
@@ -1,2 +1,3 @@
 git pull https://github.com/Sublustrum007ru/ethernetTools.git 
 git pull https://github.com/Sublustrum007ru/ethernetTools.git 
+git pull https://github.com/Sublustrum007ru/ethernetTools.git 
